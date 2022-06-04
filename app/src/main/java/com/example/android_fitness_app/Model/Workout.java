8539843f7ae860cb160d5659a425273adf6a8527
kasra <kasrahajian2002@gmail.com>
2022-06-05 02:43:49 +0430

    Add Exercise, Workout, Set model

---
+++ app/src/main/java/com/example/android_fitness_app/Model/Workout.java
@@ -0,0 +1,11 @@
+package com.example.android_fitness_app.Model;
+
+import java.util.ArrayList;
+
+public class Workout {
+    private final ArrayList<Exercise> exercises;
+
+    public Workout() {
+        this.exercises = new ArrayList<>();
+    }
+}
