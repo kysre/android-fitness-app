35a78f00bb05fa4484469967e553cdf0501f4dbf
PCMC <hm3623851@gmail.com>
2022-07-02 14:50:08 +0430

    add sideBend

---
+++ app/src/main/java/com/example/android_fitness_app/Model/allExercises/Category.java
@@ -0,0 +1,12 @@
+package com.example.android_fitness_app.Model.allExercises;
+
+public enum Category {
+    Barbell,
+    Dumbbell,
+    Machine,
+    Weighted_bodyWeight,
+    Assisted_body,
+    Reps_only,
+    Cardio_exercise,
+    Duration;
+}
