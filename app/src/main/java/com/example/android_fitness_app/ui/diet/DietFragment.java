18c31663a06fd3d77ca65d5ca5ca76be32e7ef13
kasra <kasrahajian2002@gmail.com>
2022-07-20 16:39:51 +0430

    started working on views

--- app/src/main/java/com/example/android_fitness_app/ui/diet/DietFragment.java
+++ app/src/main/java/com/example/android_fitness_app/ui/diet/DietFragment.java
@@ -1,4 +1,4 @@
-package com.example.android_fitness_app.ui.dashboard;
+package com.example.android_fitness_app.ui.diet;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,20 +12,20 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.android_fitness_app.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class DietFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        DietViewModel dietViewModel =
+                new ViewModelProvider(this).get(DietViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        dietViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/android_fitness_app/ui/training/TrainingViewModel.java
+++ app/src/main/java/com/example/android_fitness_app/ui/training/TrainingViewModel.java
@@ -1,14 +1,14 @@
-package com.example.android_fitness_app.ui.notifications;
+package com.example.android_fitness_app.ui.training;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class TrainingViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public TrainingViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
